58aff3f5825c8a43efeb52003b1f4bad2fe2b092
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-06-23 23:05:20 +1000

    Add elo rating module

--- src/components/BasicRoller.vue
+++ src/components/BasicRoller.vue
@@ -1,22 +1,36 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { DiceRoll } from '@dice-roller/rpg-dice-roller'
+import { default as EloRank } from 'elo-rank'
+
 // docs: https://www.npmjs.com/package/@dice-roller/rpg-dice-roller
 
 const result = ref(0)
 
 function newRoll() {
   const roll = new DiceRoll('2d6');
-
   result.value = roll.total;
 }
+
+const elo = new EloRank(32);
+let playerA = 1200;
+let playerB = 1400;
+
+let expectedScoreA = elo.getExpected(playerA, playerB);
+let expectedScoreB = elo.getExpected(playerB, playerA);
+ 
+//update score, 1 if won 0 if lost
+playerA = elo.updateRating(expectedScoreA, 1, playerA);
+playerB = elo.updateRating(expectedScoreB, 0, playerB);
+
+console.log(`Scores of players A, B: ${playerA}, ${playerB}`)
 
 </script>
 
 <template>
   <div class="roller">
-      <button @click="newRoll">Roll them bones</button>
-      Result: {{ result }}
+    <button @click="newRoll">Roll them bones</button>
+    Result: {{ result }}
   </div>
 </template>
 
