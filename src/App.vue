2088b159cf1b21d3cbe87b4688f23a856eeffe6e
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-07-07 00:03:13 +1000

    remove welcome splurge

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
@@ -13,7 +13,6 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
-    <TheWelcome />
 
 
   </main>
