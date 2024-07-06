f75bac130e2ee64dd1cf6210ea7863e52409f79c
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-07-07 00:21:55 +1000

    Confirm DataTest is displaying

--- src/components/DataTest.vue
+++ src/components/DataTest.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
+  import { ref } from 'vue';
+
+  const count = ref(0);
 
 </script>
 
 <template>
   <div>
-    Y
+    Testing testing
+    <button type="button" @click="count++">Count: {{ count }}</button>
   </div>
 </template>
 
