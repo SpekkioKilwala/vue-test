a84bc05c3a1c3c50a9ca04278fe0382ff81cdb43
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-07-07 17:59:29 +1000

    Add login-token code

--- src/components/DataTest.vue
+++ src/components/DataTest.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
   import { ref } from 'vue';
+  import * as Realm from "realm-web";
 
-  const count = ref(0);
+  const { MODE, PROD, DEV, SSR, BASE_URL, VITE_DATA_PASS } = import.meta.env;
+
+  const token = ref("unset");
+
+const app = new Realm.App({ id: 'data-yqnwfye' });
+async function loginEmailPassword(email:string, password:string) {
+  // Create an email/password credential
+  const credentials = Realm.Credentials.emailPassword(email, password);
+  // Authenticate the user
+  const user = await app.logIn(credentials);
+  // 'App.currentUser' updates to match the logged in user
+  console.assert(user.id === app.currentUser.id);
+  console.log(user)
+  return user;
+}
+
+async function getToken() {
+  const user = await loginEmailPassword('jonmwalsh@gmail.com', VITE_DATA_PASS);
+  return user.accessToken;
+}
 
 </script>
 
 <template>
   <div>
     Testing testing
-    <button type="button" @click="count++">Count: {{ count }}</button>
+    <button type="button" @click="() => {token = getToken() || 'waiting'}">Get token?</button>
+    {{ token }}
   </div>
 </template>
 
