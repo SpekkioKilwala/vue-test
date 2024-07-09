1a25145b20014dc97590a1c4c64cd9f3d22b3712
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-07-09 21:43:08 +1000

    Add POST with axios

--- src/components/DataTest.vue
+++ src/components/DataTest.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import { ref } from 'vue';
   import * as Realm from "realm-web";
+  import axios from 'axios';
 
   const { MODE, PROD, DEV, SSR, BASE_URL, VITE_DATA_PASS } = import.meta.env;
 
   const token = ref("unset");
+  const res = ref("nothing yet");
 
 const app = new Realm.App({ id: 'data-yqnwfye' });
 async function loginEmailPassword(email:string, password:string) {
@@ -25,12 +27,46 @@ async function getToken() {
   return user.accessToken;
 }
 
+var data = JSON.stringify({
+    "collection": "listingsAndReviews",
+    "database": "sample_airbnb",
+    "dataSource": "clusterFuffle",
+    "projection": {
+        "_id": 1
+    }
+});
+var config = {
+    method: 'post',
+    url: 'https://ap-southeast-2.aws.data.mongodb-api.com/app/data-yqnwfye/endpoint/data/v1/action/findOne',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Request-Headers': '*',
+      'Authorization': `Bearer ${ token.value }`,
+    },
+    data: data
+};
+
+async function firePost() {
+  axios(config)
+    .then(function (response) {
+        res.value = JSON.stringify(response.data);
+        console.log(res.value);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
 </script>
 
 <template>
   <div>
     <button type="button" @click="getToken">Get token?</button>
     {{ token }}
+  </div>
+  <div>
+    <button type="button" @click="firePost">This will be fine</button>
+    {{ res }}
   </div>
 </template>
 
