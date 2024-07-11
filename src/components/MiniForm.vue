28bca54e48f0f074c3567b9a29ab441cf5508934
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-07-11 20:37:31 +1000

    Add axios POST test

--- src/components/MiniForm.vue
+++ src/components/MiniForm.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import axios from 'axios';
 
-const count = ref(0)
+const responseDisplay = ref("no response yet")
 
-function inc() {
-  count.value++
+function postThing() {
+  axios.post('https://httpbin.org/post', {
+    firstName: 'Howell',
+    lastName: 'Jenkins'
+  })
+  .then(function (response) {
+    console.log(response)
+    responseDisplay.value = JSON.stringify(response.data)
+  })
+  .catch(function (error) {
+    console.log(error)
+    responseDisplay.value = JSON.stringify(error.data)
+  })
 }
 
 </script>
 
 <template>
   <div>
-    <button type="button" @click="inc">Press me</button>
+    <button type="button" @click="postThing">Press me</button>
   </div>
-  <div>
-    Count: {{ count }}
-  </div>
+  {{ responseDisplay }}
 </template>
 
 <style scoped>
