e4e5239f3683f6fcc0a28d47bc8dff7d0392be1b
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-07-14 00:12:58 +1000

    remove axios stuff

--- src/components/DataTest.vue
+++ src/components/DataTest.vue
@@ -12,55 +12,50 @@
 
   const token = ref("unset");
   const res = ref("nothing yet");
+  
+  const app = new Realm.App({ id: 'data-yqnwfye' });
+  let user = app.currentUser;
 
-const app = new Realm.App({ id: 'data-yqnwfye' });
-async function loginEmailPassword(email:string, password:string) {
-  // Create an email/password credential
-  const credentials = Realm.Credentials.emailPassword(email, password);
-  // Authenticate the user
-  const user = await app.logIn(credentials);
-  // 'App.currentUser' updates to match the logged in user
-  console.assert(user.id === app.currentUser!.id);
-  console.log(user)
-  return user;
-}
+  async function loginEmailPassword(email:string, password:string) {
+    // Create an email/password credential
+    const credentials = Realm.Credentials.emailPassword(email, password);
+    // Authenticate the user
+    const user = await app.logIn(credentials);
+    // 'App.currentUser' updates to match the logged in user
+    console.assert(user.id === app.currentUser!.id);
+    console.log(user)
+    return user;
+  }
 
-async function getToken() {
-  token.value = 'waiting...';
-  const user = await loginEmailPassword('jonmwalsh@gmail.com', VITE_DATA_PASS);
-  token.value = user.accessToken || "null";
-  return user.accessToken;
-}
+  async function getToken() {
+    token.value = 'waiting...';
+    user = await loginEmailPassword('jonmwalsh@gmail.com', VITE_DATA_PASS);
+    token.value = user.accessToken || "null";
+    return user.accessToken;
+  }
 
-var data = JSON.stringify({
-    "collection": "listingsAndReviews",
-    "database": "sample_airbnb",
-    "dataSource": "clusterFuffle",
-    "projection": {
-        "_id": 1
-    }
-});
-var config = {
-    method: 'post',
-    url: 'https://ap-southeast-2.aws.data.mongodb-api.com/app/data-yqnwfye/endpoint/data/v1/action/findOne',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Request-Headers': '*',
-      'Authorization': `Bearer <${ token.value }>`,
-    },
-    data: data
-};
+  var data = JSON.stringify({
+      "collection": "listingsAndReviews",
+      "database": "sample_airbnb",
+      "dataSource": "clusterFuffle",
+      "projection": {
+          "_id": 1
+      }
+  });
+  var config = {
+      method: 'post',
+      url: 'https://ap-southeast-2.aws.data.mongodb-api.com/app/data-yqnwfye/endpoint/data/v1/action/findOne',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'Authorization': `Bearer <${ token.value }>`,
+      },
+      data: data
+  };
 
-async function firePost() {
-  axios(config)
-    .then(function (response) {
-        res.value = JSON.stringify(response.data);
-        console.log(res.value);
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-}
+  async function firePost() {
+    console.log("do nothing")
+  }
 
 
 
